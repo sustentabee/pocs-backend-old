cf5d121131f5a18f027415db936d2273778f24a9
Gustavo Aldar <gustavoaldar@MacBook-Air-de-Gustavo.local>
2020-04-07 20:10:35 -0300

    Authentication - created

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const routes = express.Router();
 const UserController = require("./app/controllers/UserController");
+const AuthController = require("./app/controllers/AuthController");
 
 // Users
 routes.get("/users", UserController.index);
@@ -9,5 +10,8 @@ routes.get("/users/:id", UserController.show);
 routes.post("/users", UserController.store);
 routes.put("/users/:id", UserController.update);
 routes.delete("/users/:id", UserController.destroy);
+
+// Auth
+routes.post("/login", AuthController.login);
 
 module.exports = routes;
